488c87fd33376689bac95c773b221b363b398990
Kristiana Gerxhi <kgerxhi@talend.com>
2024-12-04 19:06:25 +0000

    version bump and changelog update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="tap-ga360",
-    version="0.2.0",
+    version="0.2.1",
     description="Singer.io tap for extracting data from Google Analytics 360 via BigQuery",
     author="Stitch",
     url="https://singer.io",
