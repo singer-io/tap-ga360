2ddad648268c6ee77589625fc548b168ffb32b4c
Kristiana Gerxhi <98350726+kgerxhi@users.noreply.github.com>
2025-03-11 13:31:08 -0400

    Upgrade google-cloud-bigquery (#7)
    * wip bump google-cloud-bigquery version
    -----------------------------
    
    Co-authored-by: Leslie VanDeMark <leslie.vandemark@qlik.com>
    
    * bump google-cloud-bigquery version to remove pyarrow subdependency
    -----------------------------
    
    Co-authored-by: Leslie VanDeMark <leslie.vandemark@qlik.com>
    
    ---------
    
    Co-authored-by: Leslie VanDeMark <leslie.vandemark@qlik.com>

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
     name="tap-ga360",
-    version="0.2.1",
+    version="0.2.2",
     description="Singer.io tap for extracting data from Google Analytics 360 via BigQuery",
     author="Stitch",
     url="https://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_ga360"],
-    install_requires=["google-cloud-bigquery==3.3.5", "singer-python==5.12.2", "numpy==1.26.4"],
+    install_requires=["google-cloud-bigquery==3.30.0", "singer-python==5.12.2"],
     extras_require={
         'dev': [
             'pylint',
